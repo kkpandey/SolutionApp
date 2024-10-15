f05747f8c8dc0643742a4e4ed385bcdbe989f4cb
krish <krish@KKPANDEY>
2024-10-15 19:45:39 +0530

    Add project files.

---
+++ Application/Common/Exceptions/SystemExceptions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException(string message) : base(message)
+        {
+        }
+
+        public BadRequestException(string[] errors) : base("Multiple errors occurred. See error details.")
+        {
+            Errors = errors;
+        }
+
+        public string[] Errors { get; set; }
+    }
+    public class NoDataFoundException : Exception
+    {
+        public NoDataFoundException(string message) : base(message)
+        {
+        }
+    }
+    public class SocketException : Exception
+    {
+        public SocketException(string message) : base(message)
+        {
+        }
+    }
+    public class UserAlreadyExistsException : Exception
+    {
+        public UserAlreadyExistsException() : base("User already exists.")
+        {
+        }
+    }
+}
