1be4c73973c35e7dcc1ff17846ba4d3894db6a85
Krishna kant pandey <krishnakantpandey23@gmail.com>
2024-10-16 19:28:12 +0530

    changes

---
+++ Domain/Common/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common
+{
+    public class BaseEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid ID { get; set; }
+        public DateTimeOffset DateCreated { get; set; } = DateTimeOffset.Now;
+       
+        
+    }
+}
---
+++ Domain/Entities/Employee.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Employee:BaseEntity
+    {
+        public int EmployeeId { get; set; } = 0;
+        public string CompanyName { get; set; }=string.Empty;
+        public string VerificationCode { get; set; }=string.Empty;
+
+    }
+}
